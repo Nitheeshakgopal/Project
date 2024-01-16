1adf121497815dce581659a56ebfb03f37c41120
SreekanthPDas <srreekanthpdas89@gmail.com>
2024-01-16 16:07:24 +1100

    commit

--- project/src/main/java/string/Palindrome.java
+++ project/src/main/java/string/Palindrome.java
@@ -4,9 +4,10 @@ public class Palindrome {
 
 	public static void main(String[] args) {
 		StringBuilder s1=new StringBuilder("nitheesha");
+		
 		System.out.println(s1.reverse());
-		StringBuilders3= s1.reverse;
-		StringBuilder s2=s1;
+	
+		
 		System.out.println(s2.reverse());
 		
 		
